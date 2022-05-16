401aef160f3e08bfe59ada82d8d686150f7eed6b
ashokunb <95491996+ashokunb@users.noreply.github.com>
2022-05-16 14:02:10 -0500

    having issues; with post command

--- server.js
+++ server.js
@@ -11,40 +11,56 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-function createNote(body, notesArray){
+function createNote(body, notesArray) {
     const newNote = body;
     notesArray.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
+        path.join(__dirname, "./db/db.json"),
         json.stringify({ notes: notesArray}, null, 2),
-        console.log(body)
     );
+    console.log(body);
     return newNote;
-};
+    
+}
+
+app.get("/api/notes/:id", (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+});
+
+function findById(id, noteArray) {
+    const result = noteArray.filter(note => note.id === id)[0];
+    return result;
+}
 
 
-app.get('/api/notes', (req, res) => {
-    console.log(notes, 'get request');
-    res.json(notes);
+app.get("/api/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
 });
 
 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
     req.body.id = notes.length.toString();
-    res.json(notes);
+    let notesone = createNote(req.body, notes)
+    res.json(notesone);
 });
 
 //html routes
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 
