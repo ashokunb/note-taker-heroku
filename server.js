8262e59a4c3eb69db98c67357d2a7120b1f5c999
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-16 02:59:35 -0500

    changed file structure by removing develop folder'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const { notes } = require("./Develop/db/db.json");
+const { notes } = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,14 +11,14 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 
 //html routes
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './Develop/public'))
+    res.sendFile(path.join(__dirname, './public'))
 })
 
 app.listen(PORT, ()=>{
