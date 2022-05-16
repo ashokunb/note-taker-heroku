e009b059987f90d12d899ede63960f00f16fd0f2
ashokunb <95491996+ashokunb@users.noreply.github.com>
2022-05-16 15:14:04 -0500

    changed read file for json

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const { notes } = require("./db/db.json");
+const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+console.log(notes);
 const PORT = process.env.PORT || 3001;
 
 const app = express();
